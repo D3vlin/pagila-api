7f8456bc1e5c4bf5595082674304ca70ad139940
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-07-14 05:51:27 -0500

    chore: initialize Spring Boot project

---
+++ src/test/java/co/d3vlin/pagila/api/PagilaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.d3vlin.pagila.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagilaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
